b8ec31ba5d73e34a847cfdede445842fc00901f6
unknown <iceark5@yahoo.com>
2023-01-06 14:41:22 -0600

    jwt

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -3,5 +3,8 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['ERde9C52l4mZsDgfdOOfiA==', 'EXTOb/pTICDeDGuDUQVyug==', '7qN53VQrkPzm2ClYe4KK8g==', 'xrf6PUiTFAfLkIMghXsXBg==']),
+    ADMIN_JWT_SECRET: env('ADMIN_JWT_SECRET', '/jYQHwq3mjQ2wCg30+4PNQ=='),
+    JWT_SECRET: env('JWT_SECRET', 'EzrU48j70Z4WfrZ95udgzg=='),
   },
 });
+
