c46e104745001fed46ca2890b78060631135f486
unknown <iceark5@yahoo.com>
2023-01-06 14:11:35 -0600

    fixed env

--- my-project/config/server.js
+++ my-project/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['ERde9C52l4mZsDgfdOOfiA==', 'EXTOb/pTICDeDGuDUQVyug==', '7qN53VQrkPzm2ClYe4KK8g==', 'xrf6PUiTFAfLkIMghXsXBg==']),
   },
 });
